6a0b2af02ba2fa238c148a62f4af3513fe10a895
Alexander Korzh <appmux@gmail.com>
2017-06-03 04:09:40 -0400

    Added support for colon placeholders, i.e. :key.

--- src/sprintf.js
+++ src/sprintf.js
@@ -1,11 +1,15 @@
 export default function sprintf(str, replace, ...args) {
     if (typeof replace === 'string' || !isNaN(replace)) {
-        let i = 0;
+        let i = 0
 
-        args.unshift(replace);
+        args.unshift(replace)
 
-        return str.replace(/%[dfsu]/g, match => args[i++]);
+        return str.replace(/%[dfsu]/g, match => args[i++])
     } else if (typeof replace === 'object') {
-        return str.replace(/{((?:(?!{|}).)*)}/g, (...match) => replace[match[1]] || match[0]);
+        return str
+            .replace(
+                /:([a-zA-z0-9$-]+)|{((?:(?!{|}).)+)}/g,
+                (...match) => replace[match[1] || match[2]] || match[0]
+            )
     }
 }
